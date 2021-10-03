f94ab0d83ac54aa20bd2374e3cc0034e401c7850
Veritius <45957058+Veritius@users.noreply.github.com>
2021-10-03 13:48:25 +1100

    Delete verisadminmod/lua directory

--- verisadminmod/lua/motd.lua
+++
@@ -1,6 +0,0 @@
-Hook.Add("clientConnected", "vam_MOTDHook", function(client)
-    if vam.config.motdenabled == true then
-        Game.SendDirectChatMessage(nil, vam.config.motdcontent, nil, 2, client) --Text chat
-        Game.SendDirectChatMessage(nil, vam.config.motdcontent, nil, 7, client) --Message box
-    end
-end)
